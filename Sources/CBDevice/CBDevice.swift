f8d8a2ea47159bf06fe2630e2ead1abb3363d682
Katsuya Kato <katsuya.kato@medley.jp>
2024-10-08 15:43:19 +0900

    add iPhone SE 3rd and iPhone16 series

--- Sources/CBDevice/CBDevice.swift
+++ Sources/CBDevice/CBDevice.swift
@@ -93,6 +93,7 @@ extension CBDevice {
             "iPhone14,5"  : "iPhone 13",
             "iPhone14,2"  : "iPhone 13 Pro",
             "iPhone14,3"  : "iPhone 13 Pro Max",
+            "iPhone14,6"  : "iPhone SE 3rd",
             "iPhone14,7"  : "iPhone 14",
             "iPhone14,8"  : "iPhone 14 Plus",
             "iPhone15,2"  : "iPhone 14 Pro",
@@ -101,6 +102,10 @@ extension CBDevice {
             "iPhone15,5"  : "iPhone 15 Plus",
             "iPhone16,1"  : "iPhone 15 Pro",
             "iPhone16,2"  : "iPhone 15 Pro Max",
+            "iPhone17,1"  : "iPhone 16 Pro",
+            "iPhone17,2"  : "iPhone 16 Pro Max",
+            "iPhone17,3"  : "iPhone 16",
+            "iPhone17,4"  : "iPhone 16 Plus",
 
             // iPad
             "iPad1,1": "iPad",
